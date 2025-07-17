b401c678dabee10a8e788bc2542d86b0d2a538b6
Atul <ddk801973@gmail.com>
2025-07-17 13:03:09 +0530

    Add files via upload

---
+++ Chapter-1/REPL and comments.py
@@ -0,0 +1,6 @@
+#read evaluate and move in loop formation 
+#it is used in terminal
+"""sjovsnvvnvd;lvsvsv
+svsdv
+svsvs
+vv"""
---
+++ Chapter-1/module and pip.py
@@ -0,0 +1,5 @@
+import pyjokes
+joke = pyjokes.get_joke()
+print(joke) 
+#jo khud kisi ne create kiya ho or hum usse use kre 
+#for example hum pip ka use krke terminal m pip install or kisi module ka naam likh kr usse download kr skte hai
