8486bcf6a4bfc3fa189fcea4d3666791b6d1c0bb
Atul <ddk801973@gmail.com>
2025-07-18 13:06:40 +0530

    Add files via upload

---
+++ Chapter-2/input.py
@@ -0,0 +1,16 @@
+a = int(input("Enter a number 1 :"))
+
+b = int(input("Enter a number 2 :"))
+
+print("number 1 is   ",a)
+print("number 2 is   ",b)
+print("sum of the a + b :",a+b)
+
+print("sub pf a and b is ",a-b)
+
+
+a= input("whats your name")
+b=input("what  your mother name")
+
+print("your name :",a)
+print("your mother name",b)
