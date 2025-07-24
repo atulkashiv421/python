5386f330417c17950f3d002d72bec37cd22baacc
Atul <ddk801973@gmail.com>
2025-07-24 22:30:54 +0530

    Add files via upload

---
+++ Chapter-7/for loop.py
@@ -0,0 +1,10 @@
+range= {1,2,3,4,5,6}
+for i in range:
+    print(i)
+
+
+
+x = ("harry",1,4,62,2)
+for i in x:
+    print(i)
+
---
+++ Chapter-7/whileloop.py
@@ -0,0 +1,11 @@
+# i = 1
+# while(i<19):
+#     print(i)
+#     i+=1
+
+
+b = [1,"harry","subham","kartik","atul"]
+s=0
+while(s<len(b)):
+    print(b[s])
+    s+=1
