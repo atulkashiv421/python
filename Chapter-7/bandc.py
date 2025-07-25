6f0951012770469bcfc6e1b1678e5d4a91626c11
Atul <ddk801973@gmail.com>
2025-07-26 01:28:31 +0530

    Add files via upload

---
+++ Chapter-7/bandc.py
@@ -0,0 +1,27 @@
+
+
+# for i in range(100):
+#      if i == 26:
+#       break
+#      print(i)
+
+
+for i in range(100):
+    if i == 34:
+        break
+    print(i)
+
+for i in range(100):
+    if i == 34:
+        continue
+    print(i)
+
+#PASS
+
+for i in range(0,100):
+    pass
+
+
+while(i<100):
+    print(i)
+    i+=1
